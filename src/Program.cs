e268ac0a639b03c9c3f2665e438f01c5e369a614
27SaidH <157550542+27SaidH@users.noreply.github.com>
2026-05-06 18:39:23 -0500

    Create Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+namespace cs449sprint2
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
